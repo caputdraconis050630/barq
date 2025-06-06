badeb53ed6cb8b9571ff84f6aa4d193329059b3c
caputdraconis050630 <caputdraconis@kakao.com>
2025-06-06 13:18:35 +0000

    include functions router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,14 @@
 # fastAPI Entrypoint
 
 from fastapi import FastAPI
-from app.routers import root
+from app.routers import root, functions
 from app.core.exception_handler import add_exception_handler
 
-app = FastAPI(title="Serverless Platform - Backend")
+app = FastAPI(title="Barq API")
 
 # 라우터 등록
 app.include_router(root.router)
+app.include_router(functions.router)
 
 # 예외 처리 핸들러 등록
 add_exception_handler(app)
